ece9624cefa85cad6f61d9a877a75e03a24a9ac4
vladislavs.pone <vladislavs@bilesuparadize.lv>
2021-10-31 20:27:29 +0200

    Composer update and NumberProcessorController now processes numbers

--- app/Http/Controllers/NumberProcessorController.php
+++ app/Http/Controllers/NumberProcessorController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\NumberProcessorRequest;
 use Illuminate\Http\Response;
+use NumberFormatter;
 
 class NumberProcessorController extends Controller
 {
     public function __invoke(NumberProcessorRequest $request): Response
     {
-        // TODO Process number from request
-        // TODO Add locale support
-        return response('success');
+        $result = $this->convertNumberToWords(
+            $request->get('n'),
+            $request->get('lang')
+        );
+        return response("$result\n");
+    }
+
+    protected function convertNumberToWords(string $number, string $locale): string
+    {
+        $formatter = new NumberFormatter($locale, NumberFormatter::SPELLOUT);
+        return $formatter->format($number);
     }
 }
--- app/Http/Requests/NumberProcessorRequest.php
+++ app/Http/Requests/NumberProcessorRequest.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
+
 class NumberProcessorRequest extends AbstractApiFormRequest
 {
+    public const NORMALIZED_LOCALES = [
+        'eng' => 'en',
+        'lat' => 'lv',
+    ];
+
     public function rules(): array
     {
         return [
             'n' => ['integer', 'required', 'min:0', 'max:9999'],
             'lang' => ['string', 'required', 'size:3'], // TODO check if selected locale exists
         ];
+    }
+
+    protected function passedValidation(): void
+    {
+        $validated = $this->validated();
+        $this->replace([
+            'lang' => self::NORMALIZED_LOCALES[$validated['lang']]
+        ]);
     }
 }
