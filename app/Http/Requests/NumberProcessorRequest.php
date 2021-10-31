952bdbfa432afd244726fe82ce5601f03ff1e3f1
vladislavs.pone <vladislavs@bilesuparadize.lv>
2021-10-31 20:58:47 +0200

    Added API tests

--- app/Http/Requests/NumberProcessorRequest.php
+++ app/Http/Requests/NumberProcessorRequest.php
@@ -7,6 +7,9 @@ use Illuminate\Validation\Rule;
 
 class NumberProcessorRequest extends AbstractApiFormRequest
 {
+
+    public const MIN_NUMBER = 0;
+    public const MAX_NUMBER = 9999;
     public const NORMALIZED_LOCALES = [
         'eng' => 'en',
         'lat' => 'lv',
@@ -14,8 +17,10 @@ class NumberProcessorRequest extends AbstractApiFormRequest
 
     public function rules(): array
     {
+        $minN = self::MIN_NUMBER;
+        $maxN = self::MAX_NUMBER;
         return [
-            'n' => ['integer', 'required', 'min:0', 'max:9999'],
+            'n' => ['integer', 'required', "min:$minN", "max:$maxN"],
             'lang' => ['string', 'required', 'size:3', Rule::in(array_keys(self::NORMALIZED_LOCALES))],
         ];
     }
